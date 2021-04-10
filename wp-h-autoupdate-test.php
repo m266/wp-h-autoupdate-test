21339be01a285eb37460026367e48574f1bf19f8
m266 <herbrand@mailbox.org>
2021-04-10 18:36:00 +0200

    Add files via upload

---
+++ wp-h-autoupdate-test.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name:   WP H-Autoupdate Test
+Plugin URI:    https://web266.de/software/eigene-plugins/wp-h-autoupdate-test/
+Description:   Plugin zum Testen der Update-Funktion von GitHub (nach dem Test bitte wieder entfernen)
+Author:        Hans M. Herbrand
+Author URI:    https://web266.de
+Version:       1.1
+Date:          2019-03-16
+License:       GNU General Public License v2 or later
+License URI:   http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/m266/wp-h-autoupdate-test
+*/
+
+// Externer Zugriff verhindern
+defined('ABSPATH') || exit();
+
+
+// Plugin Code
